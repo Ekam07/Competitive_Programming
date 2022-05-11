28c7ab4a9bd04872850b4768372431a4648c330b
Ekam07 <104787139+Ekam07@users.noreply.github.com>
2022-05-11 23:50:29 +0530

    Dominant army

---
+++ P9.c
@@ -0,0 +1,28 @@
+//dominant army 
+#include<stdio.h>                                                                                                                         #include <stdio.h>
+int main(void) {
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int a,b,c;
+        scanf("%d %d %d",&a,&b,&c);
+        if(a>(b+c))
+        {
+            printf("YES\n");
+        }
+        else if(c>(b+a))
+        {
+            printf("YES\n");
+        }
+        else if(b>(c+a))
+        {
+            printf("YES\n");
+        }
+        else
+        {
+            printf("NO\n");
+        }
+    }
+    return 0;
+}
